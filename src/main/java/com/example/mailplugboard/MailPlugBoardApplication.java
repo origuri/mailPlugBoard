2efd4a36dded220ee83e7ac3c5b07a1c42e87a7e
origuri <dlcksrb7@naver.com>
2023-08-01 20:51:49 +0900

    2023.8.1 - 과제 환경설정

---
+++ src/main/java/com/example/mailplugboard/MailPlugBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailplugboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailPlugBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailPlugBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mailplugboard/MailPlugBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailplugboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailPlugBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
