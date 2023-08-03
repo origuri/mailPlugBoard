2c0cad174f51d8cfb818e6cd46ab2241e2d3ae8f
origuri <dlcksrb7@naver.com>
2023-08-03 18:32:07 +0900

    2023.8.3 - post - 게시글 전체조회, 상세조회 추가

--- src/main/java/com/example/mailplugboard/controller/post/PostController.java
+++ src/main/java/com/example/mailplugboard/controller/post/PostController.java
@@ -1,5 +1,6 @@
 package com.example.mailplugboard.controller.post;
 
+import com.example.mailplugboard.model.post.PostDto;
 import com.example.mailplugboard.model.post.PostListDto;
 import com.example.mailplugboard.service.post.PostService;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("board/{boardId}")
+//@RequestMapping("board/{boardId}")
 public class PostController {
     private final PostService postService;
 
@@ -23,7 +24,7 @@ public class PostController {
      * 파라미터 : boardId
      * 해당 게시판의 게시글을 전부 가져오는 메소드
      * */
-    @GetMapping("/post")
+    @GetMapping("board/{boardId}/post")
     @ResponseBody
     public ResponseEntity postListByBoardId(@PathVariable("boardId") Long boardId){
         log.info("boardId 잘 넘어오나? -> {}", boardId);
@@ -33,6 +34,24 @@ public class PostController {
         }else{
             return new ResponseEntity("게시글 목록 조회 실패", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    /*
+     * 파라미터 : boardId, postId
+     * 게시글의 상세 내역 조회
+     * */
+    @GetMapping("board/{boardId}/post/{postId}")
+    @ResponseBody
+    public ResponseEntity postDetailByBoardIdAndPostId(@PathVariable("boardId") Long boardId,
+                                                       @PathVariable("postId") Long postId){
+        log.info("boardId, postId 잘 넘오는지 확인 -> {}, {}", boardId, postId);
+        PostDto postDto = postService.findPostByBoardIdAndPostId(boardId,postId);
+        if(postDto != null){
+            return new ResponseEntity(postDto, HttpStatus.OK);
+        }else{
+            return new ResponseEntity("게시글 상세 조회 실패", HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 
